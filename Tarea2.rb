67003f7b1de597de03c55788fb171f95295dca61
Eduardo Javier Del Valle Méndez <l20290839@cdguzman.tecnm.mx>
2024-09-05 00:42:09 +0000

    Ultima version actualizada

---
+++ Tarea2.rb
@@ -0,0 +1,36 @@
+class ContadorDePalabras
+    
+    attr_accessor :txt
+    
+    
+    def initialize(txt)# Constructor para inicializar las variables
+      @txt = txt
+    end
+  
+    # METODOS
+    def count_repeated_words_owo
+      # creamos el hash inicializado en 0, osea esta vacio
+      count = Hash.new(0)
+      
+      # le damos un barridon a las palabras
+      @txt.split.each do |word|
+       
+        word = word.downcase
+       
+        count[word] += 1
+      end
+      
+      
+      count # retornar el HASH 
+    end
+  end
+  
+ 
+  txt = "casa 1 carro 2 bicicleta 1" 
+  count = ContadorDePalabras.new(txt)
+  r = count.count_repeated_words_owo
+  
+ 
+  r.each do |word, amount| # Imprimir el resultado
+    puts "#{word}: #{amount}"
+  end
